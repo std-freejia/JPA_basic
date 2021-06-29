1f9365780bb240d9435e5390ec7ca0184a721769
std-freejia <std-freejia@gmail.com>
2021-06-29 17:56:14 +0900

    EntityManagerFactory 와 EntityManager를 열고 닫음

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -8,5 +9,12 @@ public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
+        EntityManager em = emf.createEntityManager();
+
+        // code를 작성할 위치
+
+        em.close();
+
+        emf.close();
     }
 }
