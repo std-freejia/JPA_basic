3438ebf453af1e68c173ff5f5db11a082a5be826
std-freejia <std-freejia@gmail.com>
2021-06-29 21:55:58 +0900

    [ JPQL ] JPA은 '클래스(객체)'를 대상으로 쿼리를 실행한다. (테이블 대상이 아니다) JPQL은 엔티티 객체를 대상으로 쿼리를 실행하고, SQL은 데이터베이스 테이블을 대상으로 쿼리를 실행한다. 조회 시, 페이징을 위해 시작 순서, 끝 순서를 지정할 수 있다

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,27 +4,33 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
     public static void main(String[] args) {
-        // EntityManagerFactory 를 1개 생성한다.
+        // EntityManagerFactory 를 1개 생성한다. 애플리케이션 전체에서 공유
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
-        // 트렌젝션 단위 처리마다 EntityManager가 1개씩 필요하다.
+        // 트렌젝션 단위 처리마다 EntityManager가 1개씩 필요하다. 고객의 요청 한 개에 대해 동작
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try{
-            // em : 컬렉션이라고 여겨도 된다.
+            // Member findMember = em.find(Member.class, 2L);
 
-            // find() 클래스의 식별자를 주면, 조회
-            Member findMember = em.find(Member.class, 2L);
+            /** JPA은 '클래스(객체)'를 대상으로 쿼리를 실행한다. (테이블 대상이 아니다)  */
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList(); // 페이징을 위해 시작 순서, 끝 순서를 지정할 수 있다
 
-            // 객체의 setter를 사용하기만 했는데 수정 !!
-            findMember.setName("HelloJPA");
+            for(Member member: result){
+                System.out.println("member.name = " + member.getName());
+            }
+
             tx.commit();
 
         }catch(Exception e){
