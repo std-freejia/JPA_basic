ff38575126249f1e5798ea538b327ff36bcac650
std-freejia <std-freejia@gmail.com>
2021-07-03 01:30:43 +0900

    [ 단방향 연관관계 ] 조회 시, 식별자로 다시 조회가 필요한 문제점이 있다.
    멤버 객체는 teamId만 가지고 있고 Team 객체에 대한 다른 필드를 모른다.
    찾아온 멤버가 어느 팀 소속인지 알고 싶어서, 다시 식별자로 조회 해야하는 문제점!
    
    [ 중요한 차이 ]
    * 테이블은 FK로 조인을 사용해서 연관된 테이블을 찾는다. FK하나면 있으면 그 테이블을 양쪽으로 탐색이 가능하다.
    * 객체는 '참조'를 사용해서 연관된 객체를 찾는다.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -22,12 +22,18 @@ public class JpaMain {
 
             Team team = new Team();
             team.setName("teamA");
-            em.persist(team);
+            em.persist(team); // 저장
 
             Member member = new Member();
             member.setName("member1");
             member.setTeamId(team.getId());  // 외래키 식별자를 직접 다루게 되는 문제점이 있다
             em.persist(member);
+
+            /** 찾아온 멤버가 어느 팀 소속인지 알고 싶어서, 다시 식별자로 조회 해야하는 문제점 */
+            Member findMember = em.find(Member.class, member.getId());
+
+            Long findTeamId = findMember.getTeamId();
+            Team findTeam = em.find(Team.class, findTeamId); // 계속 id로 끄집어내게 되는 문제.
 
             tx.commit(); // DB에 insert SQL 실행
         }catch(Exception e){
